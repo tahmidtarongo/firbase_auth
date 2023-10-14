7999098378a6843c43bd2a23a732ecdfc285df1e
Tahmid Tarongo <61282112+tahmidtarongo@users.noreply.github.com>
2023-10-14 12:26:13 +0600

    email,password/google/phone

--- lib/Screen/Home/home_screen.dart
+++ lib/Screen/Home/home_screen.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 
 import '../Auth/sign_in_screen.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -25,16 +26,75 @@ class _HomeScreenState extends State<HomeScreen> {
         ));
   }
 
+  CollectionReference u = FirebaseFirestore.instance.collection('users');
+
+  Future<void> postData({required String name}) async {
+    EasyLoading.show(status: 'Loading...');
+    await u.add({
+      'name': name,
+    });
+
+    EasyLoading.showSuccess('Data Post Done');
+  }
+
+  TextEditingController nameController = TextEditingController();
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    nameController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: ElevatedButton(onPressed: logOut, child: const Text("Log Out")),
       body: SafeArea(
         child: Center(
             child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisSize: MainAxisSize.min,
           children: [
-            ElevatedButton(onPressed: logOut, child: const Text("Log Out")),
-            const Text('Welcome to Home Screen'),
+            Text('Welcome to Home Screen ${FirebaseAuth.instance.currentUser?.email}'),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: TextFormField(
+                controller: nameController,
+                decoration: const InputDecoration(
+                  labelText: 'Name',
+                  hintText: 'Enter Your Name',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                postData(name: nameController.text);
+                nameController.clear();
+              },
+              child: const Text('Post Data'),
+            ),
+            FutureBuilder<QuerySnapshot>(
+              future: u.get(),
+              builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                if (snapshot.hasData) {
+                  return Padding(
+                    padding: const EdgeInsets.all(30.0),
+                    child: ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: snapshot.data?.docs.length,
+                      itemBuilder: (context, index) {
+                        Map<String, dynamic> name = snapshot.data?.docs[index].data() as Map<String, dynamic>;
+                        return Text("UserName = ${name['name']}");
+                      },
+                    ),
+                  );
+                } else {
+                  return const CircularProgressIndicator();
+                }
+              },
+            ),
           ],
         )),
       ),
